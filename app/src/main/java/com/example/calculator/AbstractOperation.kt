d1c2242ded56197f2f83ce95a97ade13d6a2335d
karwin1234 <karwin7664@gmail.com>
2024-06-10 15:07:55 +0900

    추상 클래스 수정

--- app/src/main/java/com/example/calculator/AbstractOperation.kt
+++ app/src/main/java/com/example/calculator/AbstractOperation.kt
@@ -1,6 +1,5 @@
 package com.example.calculator
 
-open class AbstractOperation() {
-
-    open fun operation(num1:Int, num2:Int):Int{return 0}
+abstract class AbstractOperation() {
+    abstract fun operation(num1:Int, num2:Int):Int
 }
